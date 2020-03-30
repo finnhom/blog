5e679301fc15a38b89e3f5df35b8520090f79179
FinnHOM <finn.omeara@forward3d.com>
2020-03-30 11:24:17 +0100

    Add like functionality

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,18 @@
+class LikesController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:comment_id])
+    @like = @comment.likes.create(article: @article)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:comment_id])
+    if @comment.likes.count != 0
+      # @comment.likes.destroy(@comment.likes.count)
+      @comment.likes.destroy_all
+    end
+    redirect_to article_path(@article)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_103305) do
+ActiveRecord::Schema.define(version: 2020_03_26_110105) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,5 +28,16 @@ ActiveRecord::Schema.define(version: 2020_03_16_103305) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "comment_id", null: false
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_likes_on_article_id"
+    t.index ["comment_id"], name: "index_likes_on_comment_id"
+  end
+
   add_foreign_key "comments", "articles"
+  add_foreign_key "likes", "articles"
+  add_foreign_key "likes", "comments"
 end
